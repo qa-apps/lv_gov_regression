bb83f4e4e1eed446f0577c2de322b9e5b113ec93
pasok <pasok.a@yahoo.com>
2020-12-15 23:45:00 +0000

    test(services): add pensions/family links and navigation smoke over first ten entries

--- cypress/e2e/services_full.cy.js
+++ cypress/e2e/services_full.cy.js
@@ -36,6 +36,44 @@ describe('Services: consolidated', () => {
     cy.contains('a', /Nekustamā īpašuma nodokļa/i).should('exist');
     cy.contains('a', /Administratīvo sodu/i).should('exist');
   });
+
+  it('pension and benefits related entries exist', () => {
+    const items = [
+      'Pensijas mantošana',
+      'Informācija par prognozēto pensiju',
+      'Pensijas pārvaldītāja izvēle',
+      'Iesniegums vecuma pensijas pārrēķināšanai',
+      'Apbedīšanas pabalsts',
+      'Iesniegums slimības pabalsta piešķiršanai',
+      'Iesniegums bezdarbnieka pabalsta piešķiršanai'
+    ];
+    items.forEach((t) => cy.contains('a, button', new RegExp(t, 'i')).should('exist'));
+  });
+
+  it('family benefits related entries exist', () => {
+    const items = [
+      'Bērna piedzimšanas pabalsts',
+      'Maternitātes',
+      'Paternitātes',
+    ];
+    items.forEach((t) => cy.contains('a, button', new RegExp(t, 'i')).should('exist'));
+  });
+
+  it('navigate to first ten service links and back (smoke)', () => {
+    cy.get('a').filter(':visible').then(($links) => {
+      const firstTen = Array.from($links).slice(0, 10);
+      firstTen.forEach((a) => {
+        const $a = Cypress.$(a);
+        const href = $a.attr('href');
+        if (href && href !== '#') {
+          cy.wrap($a).click({ force: true });
+          cy.location('pathname').should('match', /./);
+          cy.go('back');
+          cy.contains('a', /Pakalpojumi/i).should('be.visible');
+        }
+      });
+    });
+  });
 });
 
 
