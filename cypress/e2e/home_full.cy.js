d1c01071c22e3024b8273cff70f12fca565ed957
pasok <pasok.a@yahoo.com>
2020-12-15 20:55:00 +0000

    test(home): consolidated smoke tests for home and campaigns

---
+++ cypress/e2e/home_full.cy.js
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+import { HomePage } from '../pages/home';
+
+/**
+ * Consolidated home smoke:
+ * - Hero/blocks visible
+ * - Top navigation items exist
+ * - CTA links present and navigable
+ * - Promoted campaigns block
+ * - Basic a11y on main
+ */
+
+const NAV_LINKS = [
+  'Sākums',
+  'Pakalpojumi',
+  'Ko darīt',
+  'Mana Latvija.lv',
+  'E-adrese',
+  'Par portālu'
+];
+
+describe('Home: consolidated smoke', () => {
+  const home = new HomePage();
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.acceptCookies();
+  });
+
+  it('essential sections are visible', () => {
+    cy.contains('h1, h2', /Valsts pārvaldes pakalpojumu portāls/i).should('be.visible');
+    cy.contains('h2', /Visi pakalpojumi/i).should('be.visible');
+    cy.contains('h2', /Ko darīt, ja/i).should('be.visible');
+    cy.contains('h2', /Mana Latvija.lv/i).should('be.visible');
+  });
+
+  it('top navigation links are present', () => {
+    NAV_LINKS.forEach((name) => {
+      cy.get('nav').contains('a', new RegExp(name, 'i')).should('be.visible');
+    });
+  });
+
+  it('CTA links are visible and navigable', () => {
+    const ctas = [
+      'Visi pakalpojumi',
+      'Visas dzīves situācijas',
+      'Rakstīt e-adresē',
+      'Apskatīt lapas karti',
+      'Kontakti'
+    ];
+    ctas.forEach((text) => {
+      cy.contains('a, button', new RegExp(text, 'i')).should('be.visible').click({ force: true });
+      cy.location('pathname').should('match', /./);
+      cy.go('back');
+    });
+  });
+
+  it('promoted campaigns block “Pievērs uzmanību!” visible', () => {
+    cy.contains(/Pievērs uzmanību!/i).should('be.visible');
+  });
+
+  it('basic accessibility check on main content', () => {
+    cy.checkA11yBlock('main');
+  });
+});
+
+
