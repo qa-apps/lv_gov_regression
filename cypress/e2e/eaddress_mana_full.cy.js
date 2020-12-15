b5194a3a05f27b03e8b29a489f8ed9651f9d3a58
pasok <pasok.a@yahoo.com>
2020-12-15 20:10:00 +0000

    test(e-adrese+mana): consolidated checks for E-adrese and Mana Latvija.lv

---
+++ cypress/e2e/eaddress_mana_full.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+/**
+ * Consolidated E-adrese and Mana Latvija.lv checks.
+ */
+
+describe('E-adrese & Mana Latvija.lv: consolidated', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.acceptCookies();
+  });
+
+  it('E-adrese landing tiles present', () => {
+    cy.contains('a', /^E-adrese$/).click();
+    const links = [
+      'E-adreses pastkastīte',
+      'Pieslēgt e-adresi',
+      'Informācija par e-adresi',
+      'E-adreses lietošanas noteikumi'
+    ];
+    links.forEach((t) => cy.contains('a', new RegExp(t, 'i')).should('exist'));
+  });
+
+  it('Mana Latvija.lv entry and tiles', () => {
+    cy.contains('a', /Mana Latvija.lv/i).click();
+    const tiles = [
+      'E-adreses pastkastīte',
+      'Sākt lietot e-adresi',
+      'Manas darbības portālā',
+      'Profila iestatījumi'
+    ];
+    tiles.forEach((t) => cy.contains('a', new RegExp(t, 'i')).should('exist'));
+  });
+});
+
+
