22133e86659a42305241f7733e0203e776536a67
pasok <pasok.a@yahoo.com>
2020-12-15 22:00:00 +0000

    test(nav): assert EN switch visibility

--- cypress/e2e/navigation_full.cy.js
+++ cypress/e2e/navigation_full.cy.js
@@ -143,6 +143,10 @@ describe('Navigation: full coverage', () => {
   it('sign-in entry exists in header', () => {
     cy.contains('a', /Ienākt Mana Latvija.lv/i).should('be.visible');
   });
+
+  it('EN language switch visible', () => {
+    cy.contains('a,button', /^EN$/).should('be.visible');
+  });
 });
 
 
