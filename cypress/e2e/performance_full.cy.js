26e8b15dc3dac9b41e5b09b239998c5459bf2695
pasok <pasok.a@yahoo.com>
2020-12-16 01:15:00 +0000

    test(perf): add TTFB sanity and CSS asset response checks

--- cypress/e2e/performance_full.cy.js
+++ cypress/e2e/performance_full.cy.js
@@ -13,6 +13,24 @@ describe('Performance: consolidated', () => {
       expect(elapsed).to.be.lessThan(15000);
     });
   });
+
+  it('TTFB sanity via request duration', () => {
+    const before = Date.now();
+    cy.request({ url: '/', followRedirect: true }).then(() => {
+      const duration = Date.now() - before;
+      expect(duration).to.be.lessThan(5000);
+    });
+  });
+
+  it('static assets respond quickly', () => {
+    cy.request('/').then((resp) => {
+      const matches = resp.body.match(/<link[^>]+href=[\"']([^\"']+\\.css)[\"']/i);
+      if (matches && matches[1]) {
+        const href = matches[1];
+        cy.request({ url: href, failOnStatusCode: false }).its('status').should('be.oneOf', [200, 304]);
+      }
+    });
+  });
 });
 
 
