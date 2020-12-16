98411175b2fa32d150baee620f1c7a71de8448e4
pasok <pasok.a@yahoo.com>
2020-12-16 00:35:00 +0000

    test(e-adrese+mana): navigate all tiles in both sections

--- cypress/e2e/eaddress_mana_full.cy.js
+++ cypress/e2e/eaddress_mana_full.cy.js
@@ -31,6 +31,36 @@ describe('E-adrese & Mana Latvija.lv: consolidated', () => {
     ];
     tiles.forEach((t) => cy.contains('a', new RegExp(t, 'i')).should('exist'));
   });
+
+  it('navigate each E-adrese tile and back', () => {
+    cy.contains('a', /^E-adrese$/).click();
+    const links = [
+      'E-adreses pastkastīte',
+      'Pieslēgt e-adresi',
+      'Informācija par e-adresi',
+      'E-adreses lietošanas noteikumi'
+    ];
+    links.forEach((t) => {
+      cy.contains('a', new RegExp(t, 'i')).click({ force: true });
+      cy.location('pathname').should('match', /./);
+      cy.go('back');
+    });
+  });
+
+  it('navigate each Mana Latvija.lv tile and back', () => {
+    cy.contains('a', /Mana Latvija.lv/i).click();
+    const tiles = [
+      'E-adreses pastkastīte',
+      'Sākt lietot e-adresi',
+      'Manas darbības portālā',
+      'Profila iestatījumi'
+    ];
+    tiles.forEach((t) => {
+      cy.contains('a', new RegExp(t, 'i')).click({ force: true });
+      cy.location('pathname').should('match', /./);
+      cy.go('back');
+    });
+  });
 });
 
 
